eb30b5be38a0831226da019b1544bbaa74c8c523
SirSalsa <46669698+SirSalsa@users.noreply.github.com>
2023-09-11 13:45:55 +0200

    Updated url, updated depencies to latest version

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/development/',
+  base: 'https://torstenlareke.com/',
   plugins: [react()],
 })
